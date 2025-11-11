05015d8a5f9a76ba2062871c390e66b01d942faa
root <root@ute-linen.aeza.network>
2025-11-12 00:39:29 +0100

    docker update

--- config.py
+++ config.py
@@ -1,4 +1,9 @@
-BOT_TOKEN = '7652131610:AAExiWkP5DBlNJPnhGof9n0mwyAHSPU-MqA'
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 PATH_TO_DB_USERS = "db/users.db"
 
--- start_bot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-cd /root/DzhkaBotV2
-source venv/bin/activate
-python3 main.py
